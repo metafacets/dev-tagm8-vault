f544ceeadd9fdcaa1da3ed08b3f8948ce059dac5
Tony Machin <crypto@anthonymachin.com>
2014-07-26 17:02:37 +0100

    @@roots and @@folksonomy maintenance added

--- tags01.rb
+++ tags01.rb
@@ -7,6 +7,38 @@ class Tag
     @@tags
   end
 
+  def self.get_roots
+    @@roots
+  end
+
+  def self.has_root?(name)
+    @@roots.include?(name)
+  end
+
+  def self.add_root(name)
+    @@roots += [name] if !Tag.has_root?(name)
+  end
+
+  def self.delete_root(name)
+    @@roots.delete(name)
+  end
+
+  def self.get_folksonomy
+    @@folksonomy
+  end
+
+  def self.has_folksonomy?(name)
+    @@folksonomy.include?(name)
+  end
+
+  def self.add_folksonomy(name)
+    @@folksonomy += [name] if !Tag.has_folksonomy?(name)
+  end
+
+  def self.delete_folksonomy(name)
+    @@folksonomy.delete(name)
+  end
+
   def initialize(name)
     set_name(name)
     @parent = nil
@@ -24,8 +56,16 @@ class Tag
   end
 
   def self.add_tag(name, parent=nil)
-    this = Tag.has_tag?(name) ? Tag.get_tag(name) : Tag.new(name)
-    if !parent.nil?
+    if Tag.has_tag?(name)
+      new = false
+      this = Tag.get_tag(name)
+    else
+      new = true
+      this = Tag.new(name)
+    end
+    if parent.nil?
+      Tag.add_folksonomy(name) if new
+    else
       Tag.new(parent) if !Tag.has_tag?(parent)
       this.add_parent(parent)
     end
@@ -45,6 +85,13 @@ class Tag
 
   def set_parent(parent)
     @parent = parent
+    root = false
+    if Tag.has_folksonomy?(parent)
+      Tag.delete_folksonomy(parent)
+      root = true
+    end
+    Tag.add_root(parent) if root || !Tag.get_tag(parent).has_parent?
+    Tag.delete_root(get_name)
   end
 
   def has_child?(child=nil)
@@ -70,7 +117,8 @@ class Tag
   def add_parent(parent)
     if has_parent?
       if !has_parent?(parent)
-        Tag.get_tag(get_parent).delete_child(get_name)
+        tag_parent = Tag.get_tag(get_parent)
+        tag_parent.delete_child(get_name)
         set_parent(parent)
         Tag.get_tag(parent).add_child(get_name)
       end
@@ -93,7 +141,13 @@ class Tag
   end
 
   def delete_child(name)
-    @child.delete(name) if has_child?(name)
+    if has_child?(name)
+      @child.delete(name)
+      if @child.empty? && Tag.has_root?(get_name)
+        Tag.delete_root(get_name)
+        Tag.add_folksonomy(name)
+      end
+    end
   end
 
   def self.delete_tag(name)
@@ -118,10 +172,11 @@ class Tag
 end
 
 Tag.add_tag(:mouse,:animal)
-puts Tag.get_tags
+puts "Tags = #{Tag.get_tags}", "Roots = #{Tag.get_roots}", "Folks = #{Tag.get_folksonomy}"
 Tag.add_tag(:cat, :mammal)
 Tag.add_tag(:dog, :mammal)
+puts "Tags = #{Tag.get_tags}", "Roots = #{Tag.get_roots}", "Folks = #{Tag.get_folksonomy}"
 Tag.add_tag(:mammal, :animal)
-puts Tag.get_tags
+puts "Tags = #{Tag.get_tags}", "Roots = #{Tag.get_roots}", "Folks = #{Tag.get_folksonomy}"
 Tag.delete_tag(:mammal)
-puts Tag.get_tags
+puts "Tags = #{Tag.get_tags}", "Roots = #{Tag.get_roots}", "Folks = #{Tag.get_folksonomy}"
