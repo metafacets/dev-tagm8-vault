bc0a2905198e6bb225ad99382e15c868d1d8e4e8
Tony Machin <crypto@anthonymachin.com>
2014-07-30 15:36:59 +0100

    DAG support added via multiple parents

--- tags01.rb
+++ tags01.rb
@@ -40,9 +40,9 @@ class Tag
   end
 
   def initialize(name)
-    set_name(name)
-    @parent = nil
-    @child = []
+    @name = name
+    @parents = []
+    @children = []
     @items = []     # support
     @@tags[name] = self
   end
@@ -73,98 +73,96 @@ class Tag
 
   def has_parent?(parent=nil)
     if parent.nil?
-      !@parent.nil?
+      !parents.to_a.empty?
     else
-      @parent == parent
+      parents.include?(parent)
     end
   end
 
-  def get_parent
-    @parent
-  end
-
-  def set_parent(parent)
-    @parent = parent
-    root = false
-    if Tag.has_folksonomy?(parent)
-      Tag.delete_folksonomy(parent)
-      root = true
-    end
-    Tag.add_root(parent) if root || !Tag.get_tag(parent).has_parent?
-    Tag.delete_root(get_name)
+  def parents
+    @parents
   end
 
   def has_child?(child=nil)
     if child.nil?
-      !@child.to_a.empty?
+      !children.to_a.empty?
     else
-      @child.include?(child)
+      children.include?(child)
     end
   end
 
-  def get_children
-    @child
+  def children
+    @children
   end
 
-  def get_name
+  def name
     @name
   end
 
-  def set_name(name)
-    @name = name
-  end
-
   def add_parent(parent)
-    if has_parent?
-      if !has_parent?(parent)
-        tag_parent = Tag.get_tag(get_parent)
-        tag_parent.delete_child(get_name)
-        set_parent(parent)
-        Tag.get_tag(parent).add_child(get_name)
+    if !has_parent?(parent)
+      @parents += [parent]
+      root = false
+      if Tag.has_folksonomy?(parent)
+        Tag.delete_folksonomy(parent)
+        root = true
       end
-    else
-      set_parent(parent)
-      Tag.get_tag(parent).add_child(get_name)
+      Tag.add_root(parent) if root || !Tag.get_tag(parent).has_parent?
+      Tag.delete_root(name)
+      Tag.get_tag(parent).add_child(name)
     end
   end
 
-  def delete_parent
-    @parent = nil
+  def add_parents(parents)
+    @parents |= parents.to_a
   end
 
-  def add_child(name)
-    @child += [name] if !has_child?(name)
-  end
-
-  def add_children(children)
-    @child += children
-  end
-
-  def delete_child(name)
-    if has_child?(name)
-      @child.delete(name)
-      if @child.empty? && Tag.has_root?(get_name)
-        Tag.delete_root(get_name)
+  def delete_parent(parent)
+    if has_parent?(parent)
+      @parents.delete(parent)
+      tag_parent = Tag.get_tag(parent)
+      tag_parent.delete_child(name)
+      if !tag_parent.has_child? && !tag_parent.has_parent?
+        Tag.delete_root(name)
         Tag.add_folksonomy(name)
       end
     end
   end
 
-  def self.delete_tag(name)
+  def add_child(name)
+    @children += [name] if !has_child?(name)
+  end
+
+  def add_children(children)
+    @children |= children.to_a
+  end
+
+  def delete_child(name)
+    if has_child?(name)
+      @children.delete(name)
+      if @children.empty? && Tag.has_root?(name)
+        Tag.delete_root(name)
+        Tag.add_folksonomy(name)
+      end
+    end
+  end
+
+  def self.delete_tag(name) # redo
     if Tag.has_tag?(name)
       tag = Tag.get_tag(name)
-      parent = tag.get_parent
-      children = tag.get_children
-      # puts tag.get_children, parent
-      children.each do |child|
-        # puts child
-        tag_child = Tag.get_tag(child)
-        tag_child.set_parent(parent)
-      end
-      if !parent.nil?
-        tag_parent = Tag.get_tag(parent)
-        tag_parent.add_children(children)
-        tag_parent.delete_child(name)
+      parents = tag.parents
+      children = tag.children
+      if parents && children
+        parents.each do |parent|
+          tag_parent = Tag.get_tag(parent)
+          tag_parent.add_children(children)
+          tag_parent.delete_child(name)
+        end
+        children.each do |child|
+          tag_child = Tag.get_tag(child)
+          tag_child.add_parents(parents)
+          tag_child.delete_parent(name)
+         end
       end
       @@tags.delete(name)
     end
