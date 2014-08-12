c73581e25395e1aa26b00ffc217e01d604345bc5
Tony Machin <crypto@anthonymachin.com>
2014-08-12 21:50:57 +0100

    rspec instance methods context

--- tests/rspec/tags01_spec.rb
+++ tests/rspec/tags01_spec.rb
@@ -1,8 +1,9 @@
 require 'rspec'
+#require 'rspec-its'
 require 'C:\Users\anthony\Documents\My Workspaces\RubyMine\tagm8\tags01.rb'
 
 describe Tag do
-  context 'childless orphan' do
+  context 'childless orphan root and folk' do
     before(:all) do
       Tag.empty
       Tag.add_tag(:mouse, :animal)
@@ -18,8 +19,9 @@ describe Tag do
     it 'car is orphan' do
       @car.should be_orphan
     end
-    it 'car is childless_orphan' do
+    it 'car is childless_orphan or folk' do
       @car.should be_childless_orphan
+      @car.should be_folk
     end
     it 'animal is not childless' do
       @animal.should_not be_childless
@@ -27,8 +29,13 @@ describe Tag do
     it 'animal has child' do
       @animal.should be_has_child
     end
-    it 'animal is not childless orphan' do
+    it 'animal is not childless orphan or folk' do
       @animal.should_not be_childless_orphan
+      @animal.should_not be_folk
+    end
+    it 'animal is parent_orphan or root' do
+      @animal.should be_parent_orphan
+      @animal.should be_root
     end
     it 'mouse is not orphan' do
       @mouse.should_not be_orphan
@@ -36,8 +43,13 @@ describe Tag do
     it 'mouse has parent' do
       @mouse.should be_has_parent
     end
-    it 'mouse is not childless orphan' do
+    it 'mouse is not childless orphan or folk' do
       @mouse.should_not be_childless_orphan
+      @mouse.should_not be_folk
+    end
+    it 'mouse is not parent_orphan or root' do
+      @mouse.should_not be_parent_orphan
+      @mouse.should_not be_root
     end
   end
   context 'folksonomy' do
@@ -56,10 +68,10 @@ describe Tag do
       # set-up test data
       @all_childless_orphans = Tag.get_tags.values.select {|tag| tag.childless_orphan?}
     end
-    it 'includes all orphans' do
+    it 'includes all folks' do
       (@all_childless_orphans-Tag.get_folksonomy).should be_empty
     end
-    it 'includes only orphans' do
+    it 'includes only folks' do
       (Tag.get_folksonomy-@all_childless_orphans).should be_empty
     end
   end
@@ -85,6 +97,22 @@ describe Tag do
     it 'includes only roots' do
       (Tag.get_roots-@all_roots).should be_empty
     end
+  end
+  context 'instance methods' do
+    Tag.empty
+    subject {Tag.new(:my_tag)}
+    it ':name' do should respond_to(:name) end
+    it ':children' do should respond_to(:children) end
+    it ':has_child?' do should respond_to(:has_child?) end
+    it ':add children' do should respond_to(:add_children) end
+    it ':delete_child' do should respond_to(:delete_child) end
+    it ':register_child' do should respond_to(:register_child) end
+    it ':parents' do should respond_to(:parents) end
+    it ':has_parent?' do should respond_to(:has_parent?) end
+    it ':add_parents' do should respond_to(:add_parents) end
+    it ':delete_parent' do should respond_to(:delete_parent) end
+    it ':register_parent' do should respond_to(:register_parent) end
+    #its(:name){should eq(':my_tag')}
   end
   context 'instantiation' do
     before(:all) do
