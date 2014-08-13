3f8976bbb1eb88cb74df7684f9701fbcb21191be
Tony Machin <crypto@anthonymachin.com>
2014-08-13 18:44:49 +0100

    expect used instead of should (deprecated), other minor refactoring

--- tests/rspec/tags01_spec.rb
+++ tests/rspec/tags01_spec.rb
@@ -12,45 +12,17 @@ describe Tag do
       @mouse = Tag.get_tag(:mouse)
       @car = Tag.get_tag(:car)
     end
-    it 'car is childless' do
-      puts "car=#{@car}"
-      @car.should be_childless
-    end
-    it 'car is orphan' do
-      @car.should be_orphan
-    end
-    it 'car is childless_orphan or folk' do
-      @car.should be_childless_orphan
-      @car.should be_folk
-    end
-    it 'animal is not childless' do
-      @animal.should_not be_childless
-    end
-    it 'animal has child' do
-      @animal.should be_has_child
-    end
-    it 'animal is not childless orphan or folk' do
-      @animal.should_not be_childless_orphan
-      @animal.should_not be_folk
-    end
-    it 'animal is parent_orphan or root' do
-      @animal.should be_parent_orphan
-      @animal.should be_root
-    end
-    it 'mouse is not orphan' do
-      @mouse.should_not be_orphan
-    end
-    it 'mouse has parent' do
-      @mouse.should be_has_parent
-    end
-    it 'mouse is not childless orphan or folk' do
-      @mouse.should_not be_childless_orphan
-      @mouse.should_not be_folk
-    end
-    it 'mouse is not parent_orphan or root' do
-      @mouse.should_not be_parent_orphan
-      @mouse.should_not be_root
-    end
+    it 'car is childless' do expect(@car).to be_childless end
+    it 'car is orphan' do expect(@car).to be_orphan end
+    it 'car is folk' do expect(@car).to be_folk end
+    it 'animal is not childless' do expect(@animal).not_to be_childless end
+    it 'animal has child' do expect(@animal).to be_has_child end
+    it 'animal is not folk' do expect(@animal).to_not be_folk end
+    it 'animal is root' do expect(@animal).to be_root end
+    it 'mouse is not orphan' do expect(@mouse).to_not be_orphan end
+    it 'mouse has parent' do expect(@mouse).to be_has_parent end
+    it 'mouse is not folk' do expect(@mouse).to_not be_folk end
+    it 'mouse is not root' do expect(@mouse).to_not be_root end
   end
   context 'folksonomy' do
     before(:all) do
@@ -66,14 +38,12 @@ describe Tag do
       Tag.add_tag(:carpette, :carp)
       Tag.delete_tag(:mammal)
       # set-up test data
-      @all_childless_orphans = Tag.get_tags.values.select {|tag| tag.childless_orphan?}
+      all_folks = Tag.get_tags.values.select {|tag| tag.folk?}
+      @omitted_folks = (all_folks-Tag.get_folksonomy)
+      @non_folks = (Tag.get_folksonomy-all_folks)
     end
-    it 'includes all folks' do
-      (@all_childless_orphans-Tag.get_folksonomy).should be_empty
-    end
-    it 'includes only folks' do
-      (Tag.get_folksonomy-@all_childless_orphans).should be_empty
-    end
+    it 'includes all folks' do expect(@omitted_folks).to be_empty end
+    it 'includes only folks' do expect(@non_folks).to be_empty end
   end
   context 'roots' do
     before(:all) do
@@ -89,42 +59,18 @@ describe Tag do
       Tag.add_tag(:carpette, :carp)
       Tag.delete_tag(:mammal)
       # set-up test data
-      @all_roots = Tag.get_tags.values.select {|tag| tag.root?}
+      all_roots = Tag.get_tags.values.select {|tag| tag.root?}
+      @omitted_roots = (all_roots-Tag.get_roots)
+      @non_roots = (Tag.get_roots-all_roots)
     end
-    it 'includes all roots' do
-      (@all_roots-Tag.get_roots).should be_empty
-    end
-    it 'includes only roots' do
-      (Tag.get_roots-@all_roots).should be_empty
-    end
+    it 'includes all roots' do expect(@omitted_roots).to be_empty end
+    it 'includes only roots' do expect(@non_roots).to be_empty end
   end
   context 'instance methods' do
     Tag.empty
     subject {Tag.new(:my_tag)}
-    it ':name' do should respond_to(:name) end
-    it ':children' do should respond_to(:children) end
-    it ':has_child?' do should respond_to(:has_child?) end
-    it ':add children' do should respond_to(:add_children) end
-    it ':delete_child' do should respond_to(:delete_child) end
-    it ':register_child' do should respond_to(:register_child) end
-    it ':parents' do should respond_to(:parents) end
-    it ':has_parent?' do should respond_to(:has_parent?) end
-    it ':add_parents' do should respond_to(:add_parents) end
-    it ':delete_parent' do should respond_to(:delete_parent) end
-    it ':register_parent' do should respond_to(:register_parent) end
-    #its(:name){should eq(':my_tag')}
+    methods = [:name,:children,:has_child?,:add_children,:delete_child,:register_child,:parents,:has_parent?,:add_parents,:delete_parent,:register_parent]
+    methods.each {|method| it method do expect(subject).to respond_to(method) end }
+    it ':name ok' do expect(subject.name).to eq(:my_tag) end
   end
-  context 'instantiation' do
-    before(:all) do
-      Tag.empty
-      @tag = Tag.new(:mammal)
-    end
-    it 'initialises' do
-      @tag.should be_an_instance_of Tag
-    end
-    it 'has supplied name' do
-      @tag.name.should == :mammal
-    end
-  end
-
 end
