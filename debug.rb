343b76d691bc783ad675c43fba314450e48318c9
Tony Machin <crypto@anthonymachin.com>
2014-08-20 19:42:47 +0100

    refined output formatting - tags still unsupported

--- debug.rb
+++ debug.rb
@@ -6,13 +6,7 @@ class Debug
       output.process(options)
     end
   end
-  attr_accessor :class, :method, :note, :vars, :level
-  def initialize(options={})
-    options.each {|k,v| instance_variable_set("@#{k}", v)}
-    @@outputs |= [self]
-#    puts "Debug.new: @@outputs=#{@@outputs}"
-  end
-  def normalize(options)
+  def self.normalize(options)
     default = {class:nil,method:nil,note:nil,vars:nil,level:0}
     options = default.merge(options)
 #    puts "Debug.standardise 1: options=#{options}"
@@ -26,7 +20,7 @@ class Debug
                             [crit.to_s]
                           end
     end
-    puts "Debug.standardise 2: options=#{options}"
+#    puts "Debug.standardise 2: options=#{options}"
     vars = if options[:vars].kind_of?(Array)
              if options[:vars][0].kind_of?(Array)
                options[:vars]
@@ -40,12 +34,18 @@ class Debug
            end
     options[:vars] = vars
     options[:level] = [options[:level]]
-    puts "Debug.standardise 3: options=#{options}"
+#    puts "Debug.standardise 3: options=#{options}"
     options
+  end
+  attr_accessor :class, :method, :note, :vars, :level
+  def initialize(options={})
+    options.each {|k,v| instance_variable_set("@#{k}", v)}
+    @@outputs |= [self]
+#    puts "Debug.new: @@outputs=#{@@outputs}"
   end
   def process(options)
     # options.normalize!
-    options = normalize(options)
+    options = Debug.normalize(options)
     catch :done do
       options.each do |k, v|
 #        puts "Debug.process 1: v=#{v}"
@@ -63,12 +63,20 @@ class Debug
   end
   def show(options)
     puts "Debug.show 1: options=#{options}"
-    out = options[:level][0] > 0 ? "#{'  '*options[:level][0]}" : ''
-    [:class,:method,:note].each do |option|
-      out += ", #{option.to_s}=#{options[option][0]}" unless options[option].empty?
+    out = options[:level][0] > 0 ? "#{'^'*options[:level][0]}" : ''
+    [:class,:method].each {|option| out += ".#{options[option][0]}" unless options[option].empty?}
+    unless options[:note].empty?
+      quote = options[:note][0].match(/^[0-9]+$/) ? '' : '"'
+      out += " #{quote}#{options[:note][0]}#{quote}"
     end
-    options[:vars].each { |var| out += ", #{var[0]}=#{var[1]}" }
-#    puts "Debug.show 2: out=#{out}"
-    puts out.gsub(/^, /,"")
+    out.gsub!(/^(\^*)(\.)(.*)/,'\1\3')
+    out.gsub!(/\^/,' ')
+    out.gsub!(/^ *$/,'')
+    out += ': ' unless out.empty?
+    vars = ''
+    options[:vars].each { |var| vars += ", #{var[0]}=#{var[1]}" }
+    out += vars.gsub(/^, /,'')
+    puts "Debug.show 2: out=#{out}"
+    puts out
   end
 end
