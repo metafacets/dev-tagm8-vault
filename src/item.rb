ef86e31b0aa0b143df22e887b7acaf2b6ee2b36f
Tony Machin <crypto@anthonymachin.com>
2014-09-29 15:28:54 +0100

    tests expanded

---
+++ src/item.rb
@@ -0,0 +1,86 @@
+require_relative 'debug.rb'
+require_relative 'tag.rb'
+
+#Debug.new(class:'Tag') # comment out to turn off
+#Debug.new(method:'abstract')
+
+class Item
+
+  def self.taxonomy=(taxonomy) @@taxonomy = taxonomy end
+  def self.taxonomy; @@taxonomy; end
+  def self.items=(items) @@items = items end
+  def self.items; @@items end
+
+  attr_accessor :date, :name, :content, :tags, :sees
+
+  Item.items = []
+
+  def initialize(entry=nil)
+    @date = Time.now
+    @name = ''
+    @content = ''
+    @tags = []
+    @sees = []
+    instantiate(entry)
+  end
+
+  def instantiate(entry)
+    unless !entry.is_a? String || entry.nil? || entry.empty?
+      parse(entry)
+      Item.items |= [self]
+    end
+  end
+
+  def parse(entry)
+    parse_entry(entry)
+    parse_content
+  end
+
+  def parse_entry(entry)
+    # gets @name and @content
+    first, *rest = entry.split("\n")
+    puts "parse_entry 1: first=#{first}, rest=#{rest}"
+    @name = first if first
+    puts "parse_entry 2: name=#{name}"
+    if rest
+      @content = rest.join("\n")
+      puts "parse_entry 3: content=#{content}"
+    end
+  end
+
+  def parse_content
+    # gets content tags
+    # + or - solely instantiate or deprecate the taxonomy
+    # otherwise taxonomy gets instantiated and item gets tagged by its leaves
+    unless content.empty?
+      content.scan(/([+|-|=]?)#([^\s]+)/).each do |op,tag_ddl|
+        puts "parse_tags 1: op=#{op}, tag_ddl=#{tag_ddl}"
+        if op == '-'
+          @tags -= Item.taxonomy.deprecate(tag_ddl)
+          puts "parse_tags 2a: tags=#{tags}, Item.taxonomy.tags=#{Item.taxonomy.tags}"
+        else
+          leaves = Item.taxonomy.instantiate(tag_ddl)
+          puts "parse_tags 2b: leaves=#{leaves}"
+          if op == '' || op == "="
+            leaves.each {|tag| tag.items |= [self]}
+            @tags |= leaves
+            puts "parse_tags 3: tags=#{tags}, Item.taxonomy.tags=#{Item.taxonomy.tags}"
+          end
+        end
+      end
+    end
+  end
+end
+
+tax = Taxonomy.new
+#tax.instantiate('[:cat,:dog]<:mammal')
+#puts tax.tags
+Item.taxonomy = tax
+item = Item.new("Item 1\n+#[mammal,fish]<:animal>[insect,bird>[parrot,eagle]]\nMy entry =#cat,fish #:dog for my cat and dog")
+puts "item=#{item}, date=#{item.date}, name=#{item.name}, content=#{item.content}, tags=#{item.tags}"
+puts "tax.tags=#{tax.tags}"
+
+
+
+
+
---
+++ tests/rspec/item_spec.rb
@@ -0,0 +1,105 @@
+require 'rspec'
+require 'C:\Users\anthony\Documents\My Workspaces\RubyMine\tagm8\src\item.rb'
+
+describe Item do
+  describe 'instance methods' do
+    item = Item.new
+    subject {item}
+    methods = [:date, :name, :content, :tags, :sees, :instantiate, :parse, :parse_entry, :parse_content]
+    methods.each {|method| it method do expect(subject).to respond_to(method) end }
+  end
+  describe :instantiate do
+    # test = [[entry,name,content,tags,tax.tags]]
+    tests = [["Name\nContent","Name","Content",[],[],[]]\
+            ,["Name\nContent\ncont","Name","Content\ncont",[],[]]\
+            ,["Name\n#a Content","Name","#a Content",[:a],[:a]]\
+            ,["Name\nContent #a","Name","Content #a",[:a],[:a]]\
+            ,["Name\nContent #:a","Name","Content #:a",[:a],[:a]]\
+            ,["Name\nContent =#a","Name","Content =#a",[:a],[:a]]\
+            ,["Name\nContent =#:a","Name","Content =#:a",[:a],[:a]]\
+            ,["Name\nContent +#a","Name","Content +#a",[],[:a]]\
+            ,["Name\nContent +#:a","Name","Content +#:a",[],[:a]]\
+            ,["Name\nContent\n#a","Name","Content\n#a",[:a],[:a]]\
+            ,["Name\nContent\ncont #a","Name","Content\ncont #a",[:a],[:a]]\
+            ,["Name\nContent\n#a cont","Name","Content\n#a cont",[:a],[:a]]\
+            ,["Name\nContent #a #b","Name","Content #a #b",[:a,:b],[:a,:b]]\
+            ,["Name\nContent #a\n#b","Name","Content #a\n#b",[:a,:b],[:a,:b]]\
+            ,["Name\nContent #:a<:b\ncont","Name","Content #:a<:b\ncont",[:a],[:a,:b]]\
+            ,["Name\nContent =#:a<:b\ncont","Name","Content =#:a<:b\ncont",[:a],[:a,:b]]\
+            ,["Name\nContent +#:a<:b\ncont","Name","Content +#:a<:b\ncont",[],[:a,:b]]\
+            ,["Name\nContent #:a>[:b,:c>[:d,:e]]\ncont","Name","Content #:a>[:b,:c>[:d,:e]]\ncont",[:b,:d,:e],[:a,:b,:c,:d,:e]]\
+            ,["Name\nContent #:a>[:b,:c>[:d,:e]]\ncont #a,b,f","Name","Content #:a>[:b,:c>[:d,:e]]\ncont #a,b,f",[:a,:b,:d,:e,:f],[:a,:b,:c,:d,:e,:f]]\
+            ,["Name\nContent #a,b,f\ncont #:a>[:b,:c>[:d,:e]]","Name","Content #a,b,f\ncont #:a>[:b,:c>[:d,:e]]",[:a,:b,:d,:e,:f],[:a,:b,:c,:d,:e,:f]]\
+            ,["Name\nContent #:a>[:b,:c>[:d,:e]]\ncont #a #b,f","Name","Content #:a>[:b,:c>[:d,:e]]\ncont #a #b,f",[:a,:b,:d,:e,:f],[:a,:b,:c,:d,:e,:f]]\
+            ,["Name\nContent #:a>[:b,:c>[:d,:e]]\ncont #a #b,f","Name","Content #:a>[:b,:c>[:d,:e]]\ncont #a #b,f",[:a,:b,:d,:e,:f],[:a,:b,:c,:d,:e,:f]]\
+            ,["Name\n#a Content #:a>[:b,:c>[:d,:e]]\ncont #b,f","Name","#a Content #:a>[:b,:c>[:d,:e]]\ncont #b,f",[:a,:b,:d,:e,:f],[:a,:b,:c,:d,:e,:f]]\
+            ,["Name\n#b,f Content #:a>[:b,:c>[:d,:e]]\ncont #a","Name","#b,f Content #:a>[:b,:c>[:d,:e]]\ncont #a",[:a,:b,:d,:e,:f],[:a,:b,:c,:d,:e,:f]]\
+            ]
+    tests.each do |test|
+      describe test[0] do
+        describe :parse_entry do
+          tax = Taxonomy.new
+          Item.taxonomy = tax
+          Item.items = []
+          item = Item.new
+          item.parse_entry(test[0])
+          it "name = #{test[1]}" do expect(item.name).to eq(test[1]) end
+          it "content = #{test[2]}" do expect(item.content).to eq(test[2]) end
+        end
+        describe :parse_content do
+          tax = Taxonomy.new
+          Item.taxonomy = tax
+          Item.items = []
+          item = Item.new
+          item.name = test[1]
+          item.content = test[2]
+          item.parse_content
+          item_tags = item.tags.map {|tag| tag.name.to_sym}.sort
+          it "tags = #{test[3]}" do expect(item_tags).to eq(test[3]) end
+        end
+        describe :parse do
+          tax = Taxonomy.new
+          Item.taxonomy = tax
+          Item.items = []
+          item = Item.new
+          item.parse(test[0])
+          item_tags = item.tags.map {|tag| tag.name.to_sym}.sort
+          tax_tags = tax.tags.keys.sort
+          it "name = #{test[1]}" do expect(item.name).to eq(test[1]) end
+          it "content = #{test[2]}" do expect(item.content).to eq(test[2]) end
+          it "tags = #{test[3]}" do expect(item_tags).to eq(test[3]) end
+          it "Taxonomy.tags = #{test[4]}" do expect(tax_tags).to eq(test[4]) end
+        end
+        describe :instantiate do
+          tax = Taxonomy.new
+          Item.taxonomy = tax
+          Item.items = []
+          item = Item.new
+          item.instantiate(test[0])
+          item_tags = item.tags.map {|tag| tag.name.to_sym}.sort
+          tax_tags = tax.tags.keys.sort
+          items = Item.items.map {|i| i.name}
+          it "name = #{test[1]}" do expect(item.name).to eq(test[1]) end
+          it "content = #{test[2]}" do expect(item.content).to eq(test[2]) end
+          it "tags = #{test[3]}" do expect(item_tags).to eq(test[3]) end
+          it "Taxonomy.tags = #{test[4]}" do expect(tax_tags).to eq(test[4]) end
+          it "items = ['Name']" do expect(items).to eq(['Name']) end
+        end
+        describe :initialize do
+          tax = Taxonomy.new
+          Item.taxonomy = tax
+          Item.items = []
+          item = Item.new(test[0])
+          item_tags = item.tags.map {|tag| tag.name.to_sym}.sort
+          tax_tags = tax.tags.keys.sort
+          items = Item.items.map {|i| i.name}
+          it "name = #{test[1]}" do expect(item.name).to eq(test[1]) end
+          it "content = #{test[2]}" do expect(item.content).to eq(test[2]) end
+          it "tags = #{test[3]}" do expect(item_tags).to eq(test[3]) end
+          it "Taxonomy.tags = #{test[4]}" do expect(tax_tags).to eq(test[4]) end
+          it "items = ['Name']" do expect(items).to eq(['Name']) end
+        end
+      end
+    end
+  end
+end
